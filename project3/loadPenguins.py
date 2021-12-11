f70e78440a145051ad1507a853005af5165f1df7
Jørn-Marcus Høylo-Rosenberg <jornmarh@uio.no>
2021-12-11 21:43:30 +0100

    update

--- project3/loadPenguins.py
+++ project3/loadPenguins.py
@@ -2,9 +2,21 @@ import pandas as pd
 import seaborn as sns
 from palmerpenguins import load_penguins
 
-sns.set_style("whitegrid")
+data = load_penguins()
+print(data.shape)
+print(data.columns)
+print(data)
 
-x, y = load_penguins(return_X_y=True)
-print(y)
-#print(x)
-#print(y.shape)
+
+#Check for nan elements
+print(data.isna().sum())
+#Remove rows containing nan elements
+data.dropna(inplace=True)
+print(data.isna().sum())
+print(data.shape)
+
+body = data.loc[:, ["bill_length_mm", "bill_depth_mm", "flipper_length_mm", "body_mass_g"]]
+print(body.head())
+
+penguins = data["species"]
+print(penguins.head())
